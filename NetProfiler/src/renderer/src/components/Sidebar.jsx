4f0b9667479190dc2920d759b15a961aa3d35762
sahbahgonzalez <116740133+sahbahgonzalez@users.noreply.github.com>
2025-01-16 18:16:54 -0500

    Update Sidebar.jsx added editor sidebar and import/export buttons

--- NetProfiler/src/renderer/src/components/Sidebar.jsx
+++ NetProfiler/src/renderer/src/components/Sidebar.jsx
@@ -1,28 +1,42 @@
 import React from 'react';
-import HSI_logo from '../assets/HSI_logo.png'
-import FGCU_logo from '../assets/FGCU_logo.jpg'
+import HSI_logo from '../assets/HSI_logo.png';
+import FGCU_logo from '../assets/FGCU_logo.jpg';
 
 const Sidebar = ({ activeTab }) => {
-  return (
-    <div className="sidebar">
-      {activeTab === 'My Cards' && (
-        <div>
-          <input type="text" className="search-bar" placeholder="Search cards" />
-          <div className="button-container">
-            <button className="new-card-button">New Card</button>
-            <button className="new-deck-button">New Deck</button>
-          </div>
-          <div className="logo-container">
-            <img alt="hsi logo" className='logo' src={HSI_logo} />
-            <img alt="fgcu logo" className='logo' src={FGCU_logo} />
-          </div> 
+    return (
+        <div className="sidebar">
+            {activeTab === 'My Cards' && (
+                <div>
+                    <input type="text" className="search-bar" placeholder="Search cards" />
+                    <div className="button-container">
+                        <button className="new-card-button">New Card</button>
+                        <button className="new-deck-button">New Deck</button>
+                    </div>
+                    <div className="logo-container">
+                        <img alt="hsi logo" className="logo" src={HSI_logo} />
+                        <img alt="fgcu logo" className="logo" src={FGCU_logo} />
+                    </div>
+                </div>
+            )}
+            {activeTab === 'Editor' && (
+                <div>
+                    <input type="text" className="search-bar" placeholder="Search items" />
+                    <div className="file-buttons-container">
+                        <button className="file-button">
+                            <span className="icon">📄</span> Import File
+                        </button>
+                        <button className="file-button">
+                            <span className="icon">📂</span> Export File
+                        </button>
+                    </div>
+                    <div className="logo-container">
+                        <img alt="hsi logo" className="logo" src={HSI_logo} />
+                        <img alt="fgcu logo" className="logo" src={FGCU_logo} />
+                    </div>
+                </div>
+            )}
         </div>
-      )}
-      {activeTab === 'Editor' && (
-        <div className="editor-sidebar">editor sidebar stuff here</div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default Sidebar;

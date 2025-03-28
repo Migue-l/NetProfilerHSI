a7098c156d0bcc058c0510928bcb13b1804988fd
Jr16H <howardjonathan2002@gmail.com>
2025-03-28 16:54:12 -0400

    moved the save button (miguel did it all)

--- src/renderer/src/components/MainContent.jsx
+++ src/renderer/src/components/MainContent.jsx
@@ -409,7 +409,7 @@ const MainContent = ({ activeTab, newCardData, setSelectedDirectory, setDecks, s
                                     </div>
                                 </div>
                                 <div className="category-editing-panel">
-                                    <button className="save-button" onClick={handleSaveSubcatData}>Save Card</button>
+                                    <button className="preview-button" onClick={handleSaveSubcatData}>Save Card</button>
                                     <button className="preview-button" onClick={handlePreviewClick}>Preview Card</button>
                                     {openCards[activeCardIndex].selectedCategory ? (
                                         <>
